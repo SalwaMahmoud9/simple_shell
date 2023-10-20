ce9378f3f27d595cfc61f350e063d9ffa7799e20
Asmaa Hesham <108579670+Asmaa26189@users.noreply.github.com>
2023-10-20 23:07:12 +0300

    20-10-2023

--- main.c
+++ main.c
@@ -11,16 +11,17 @@ int main(int arg_count, char **arg_vector)
 
 	int file_desc = 2;
 	information_struct information[] = { INFORM_INI };
-	
+	int check0 = 0, check1 = 1, check2 = 2, check_1 = -1;
+
 	asm ("mov %1, %0\n\t"
 		"add $3, %0"
 		: "=r" (file_desc)
 		: "r" (file_desc));
 
-	if (arg_count == 2)
+	if (arg_count == check2)
 	{
-		file_desc = open(arg_vector[1], O_RDONLY);
-		if (file_desc == -1)
+		file_desc = open(arg_vector[check1], O_RDONLY);
+		if (file_desc == check_1)
 		{
 			if (errno == EACCES)
 			{
@@ -28,9 +29,9 @@ int main(int arg_count, char **arg_vector)
 			}
 			if (errno == ENOENT)
 			{
-				put_estring(arg_vector[0]);
+				put_estring(arg_vector[check0]);
 				put_estring(": 0: Cannot open ");
-				put_estring(arg_vector[1]);
+				put_estring(arg_vector[check1]);
 				put_echar('\n');
 				put_echar(BUFF_F);
 				exit(127);
