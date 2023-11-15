53ed4a403504dadc03cc8161cb3da7be3e09d2ff
Asmaa26189 <engasmaahesham@gmail.com>
2023-11-16 01:09:26 +0200

    16-11-2023

--- main.c
+++ main.c
@@ -10,24 +10,25 @@ int main(int c, char **v)
 {
 	int fileD = 2;
 	info_Pass info[] = { INFO_INIT };
+	int x = 0, y = 1, z = 2, u = -1;
 
 	asm ("mov %1, %0\n\t"
 		"add $3, %0"
 		: "=r" (fileD)
 		: "r" (fileD));
 
-	if (c == 2)
+	if (c == z)
 	{
-		fileD = open(v[1], O_RDONLY);
-		if (fileD == -1)
+		fileD = open(v[y], O_RDONLY);
+		if (fileD == u)
 		{
 			if (errno == EACCES)
 				exit(126);
 			if (errno == ENOENT)
 			{
-				_put(v[0]);
+				_put(v[x]);
 				_put(": 0: Can't open ");
-				_put(v[1]);
+				_put(v[y]);
 				_putC('\n');
 				_putC(BUFFER_FLUSH);
 				exit(127);
